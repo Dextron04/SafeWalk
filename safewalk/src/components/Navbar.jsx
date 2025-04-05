9e48186c8f9bf48ddbefe2596ee3da1e828cec99
MalavyaRaval <malavyaraval@gmail.com>
2025-04-04 19:26:40 -0700

    fixed smart-routes

--- safewalk/src/components/Navbar.jsx
+++ safewalk/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,22 +8,44 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4 shadow-md z-50 relative">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-yellow-400">SafeWalk</Link>
+        <Link to="/" className="text-2xl font-bold text-yellow-400">
+          SafeWalk
+        </Link>
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-white md:hidden focus:outline-none"
         >
           ☰
         </button>
-        <div className={`md:flex md:items-center md:space-x-6 ${isOpen ? 'block' : 'hidden'}`}>
-          <Link to="/map" className="block mt-2 md:mt-0 hover:text-yellow-300">Map</Link>
-          <Link to="/statistics" className="block mt-2 md:mt-0 hover:text-yellow-300">Statistics</Link>
-          <Link to="/help" className="hover:text-yellow-300">Help</Link>
-          <Link to="/alerts" className="hover:text-yellow-300">Alerts</Link>
-          <Link to="/smart-routes" className="block mt-2 md:mt-0 hover:text-yellow-300">
-                            Smart Routes</Link>
-          <Link to="/feedback" className="hover:text-yellow-300">Feedback</Link>
-
+        <div
+          className={`md:flex md:items-center md:space-x-6 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <Link to="/map" className="block mt-2 md:mt-0 hover:text-yellow-300">
+            Map
+          </Link>
+          <Link
+            to="/statistics"
+            className="block mt-2 md:mt-0 hover:text-yellow-300"
+          >
+            Statistics
+          </Link>
+          <Link to="/help" className="hover:text-yellow-300">
+            Help
+          </Link>
+          <Link to="/alerts" className="hover:text-yellow-300">
+            Alerts
+          </Link>
+          <Link
+            to="/routes"
+            className="block mt-2 md:mt-0 hover:text-yellow-300"
+          >
+            Smart Routes
+          </Link>
+          <Link to="/feedback" className="hover:text-yellow-300">
+            Feedback
+          </Link>
         </div>
       </div>
     </nav>
